726965cc3bbfc97185d26e0c08d5d9b14eced922
huyim <huym0625@gmail.com>
2023-08-30 17:18:44 +0200

    Finished lesson 3 basic scene

--- 03-Basic-scene/script.js
+++ 03-Basic-scene/script.js
@@ -1,1 +1,33 @@
-console.log("Hello");
+//Scene
+const scene = new THREE.Scene();
+
+//Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube_mesh = new THREE.Mesh(geometry, material);
+scene.add(cube_mesh);
+
+//Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+//Camera
+//The field of view (fov), in degrees; degree越大离物体越近
+//The aspect ratio, width of the render divided by the height of the render
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// forward/backward in z axis
+camera.position.z = 3;
+camera.position.x = 2;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+//console.log(canvas);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
